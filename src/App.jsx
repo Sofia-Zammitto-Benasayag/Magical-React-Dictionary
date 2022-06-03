ff65a975ae1d1b27e9b46a56b1fedf89491265a5
Sofia L. Zammitto Benasayag <76660573+Sofia-Zammitto-Benasayag@users.noreply.github.com>
2022-06-03 22:17:58 +0000

    Created search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
+    <div>
     <main>
-      React⚛️ + Vite⚡ + Replit🌀
+      <Dictionary />
     </main>
+    <footer> Coded with magic by
+      <a href="#"> Sofia </a> 
+    </footer>
+    </div>  
   );
 }
 
